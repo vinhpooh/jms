232f6bae2ba0bfd3bb1d70a20f1930eb1e4e590b
vinhpooh <pham.hoang.vinh@gmail.com>
2017-03-24 17:14:38 +0100

    recuperation du port via argument

--- portail-jms/src/main/java/fr/pham/vinh/portail/PortailJmsLauncher.java
+++ portail-jms/src/main/java/fr/pham/vinh/portail/PortailJmsLauncher.java
@@ -1,6 +1,7 @@
 package fr.pham.vinh.portail;
 
 import fr.pham.vinh.portail.jms.PortailJms;
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -23,13 +24,12 @@ public class PortailJmsLauncher {
 
     private final static String[] SCAN_PACKAGES = new String[]{"fr.pham.vinh.portail.resource"};
     private final static String DEFAULT_CONTEXT_PATH = "/portail-jms/*";
-    private static final String DEFAULT_PORT = "8080";
+    private static final int DEFAULT_PORT = 8080;
     private static final String DEFAULT_TOPIC = "topic.portail.qi";
     private static final String DEFAULT_USER = "admin";
     private static final String DEFAULT_PASSWORD = "admin123";
 
     private static String contextPath;
-    private static int port;
     private static String topic;
     private static String user;
     private static String password;
@@ -39,20 +39,19 @@ public class PortailJmsLauncher {
         try (InputStream in = PortailJmsLauncher.class.getClassLoader().getResourceAsStream("properties.properties")) {
             properties.load(in);
             contextPath = properties.getProperty("context.path", DEFAULT_CONTEXT_PATH);
-            port = Integer.valueOf(properties.getProperty("port", DEFAULT_PORT));
             topic = properties.getProperty("topic", DEFAULT_TOPIC);
             user = properties.getProperty("user", DEFAULT_USER);
             password = properties.getProperty("password", DEFAULT_PASSWORD);
         } catch (IOException e) {
             LOGGER.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } catch (NumberFormatException e) {
-            LOGGER.warn("Cannot parse port \"{}\", using default port \"{}\".", properties.getProperty("port"), DEFAULT_PORT);
-            port = Integer.valueOf(DEFAULT_PORT);
         }
     }
 
     public static void main(String args[]) throws Exception {
+        // Get port from args
+        int port = args.length != 0 && StringUtils.isNotEmpty(args[0]) ? Integer.valueOf(args[0]) : DEFAULT_PORT;
+
         // ResourceConfig (Jersey) inherit from Application and gives a new configuration without custom properties.
         // The next line takes a vararg of packages as arguments to search for components / resources.
         ResourceConfig config = new ResourceConfig();
