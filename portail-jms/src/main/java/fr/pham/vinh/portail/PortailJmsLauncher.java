53b0670dc921d5c5efb45d67d402b043a7ef8868
vinhpooh <pham.hoang.vinh@gmail.com>
2017-03-29 17:16:37 +0200

    deplacement du serveur web embarque

--- portail-jms/src/main/java/fr/pham/vinh/portail/PortailJmsLauncher.java
+++ portail-jms/src/main/java/fr/pham/vinh/portail/PortailJmsLauncher.java
@@ -1,13 +1,8 @@
 package fr.pham.vinh.portail;
 
 import fr.pham.vinh.portail.jms.PortailJms;
+import fr.pham.vinh.portail.server.EmbeddedServer;
 import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,7 +18,6 @@ public class PortailJmsLauncher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PortailJmsLauncher.class);
 
-    private final static String[] SCAN_PACKAGES = new String[]{"fr.pham.vinh.portail.resource"};
     private static final int DEFAULT_PORT = 8080;
     private static final String DEFAULT_TOPIC = "topic.portail.qi";
     private static final String DEFAULT_USER = "admin";
@@ -50,44 +44,13 @@ public class PortailJmsLauncher {
         // Get port from args
         int port = args.length != 0 && StringUtils.isNotEmpty(args[0]) ? Integer.valueOf(args[0]) : DEFAULT_PORT;
 
-        // ResourceConfig (Jersey) inherit from Application and gives a new configuration without custom properties.
-        // The next line takes a vararg of packages as arguments to search for components / resources.
-        ResourceConfig config = new ResourceConfig();
-        config.packages(SCAN_PACKAGES);
-
-        // The ServletContainer (Jersey) is a Servlet / Filter for deploying root resource classes.
-        ServletContainer servletContainer = new ServletContainer(config);
-
-        // Server (Jetty) is the main class for the Jetty HTTP Servlet server. It aggregates connectors and requests.
-        // The server itself is a handler and a ThreadPool.
-        Server server = new Server(port);
-
-        // Setup the basic application "context" for this application at "/"
-        // This is also known as the handler tree (in jetty speak)
-        ServletContextHandler context = new ServletContextHandler(server, "/");
-        context.setResourceBase(PortailJmsLauncher.class.getResource("/webroot").toExternalForm());
-        context.setWelcomeFiles(new String[]{"index.html"});
-
-        // Add Jersey servlet at "/portail-jms/*"
-        ServletHolder portailJmsHolder = new ServletHolder("portail-jms", servletContainer);
-        context.addServlet(portailJmsHolder, "/portail-jms/*");
-
-        // Lastly, the default servlet for root content (always needed, to satisfy servlet spec)
-        // Must be named "default", must be on path mapping "/" and it is important that this is last.
-        ServletHolder holderPwd = new ServletHolder("default", DefaultServlet.class);
-        holderPwd.setInitParameter("dirAllowed", "false");
-        context.addServlet(holderPwd, "/");
-
         try (PortailJms portailJms = new PortailJms(topic, user, password)) {
             // Initialize publiser and subscriber
             portailJms.init();
-            // Start things up!
-            server.start();
-            LOGGER.info("Jetty server is reachable on port {}", port);
-            // By using the server.join() the server thread will join with the current thread.
-            server.join();
-        } finally {
-            server.destroy();
+
+            // Start the embedded server
+            EmbeddedServer embeddedServer = new EmbeddedServer(port);
+            embeddedServer.start();
         }
     }
 
